36f1cd7875834a2f7a08329018c4d8b554bbe5e5
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-12-14 22:32:26 +0100

    initial commit

---
+++ bin/dist.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+SRC_DIR=src/logo
+DIST_DIR=dist
+
+mkdir -p "$DIST_DIR"
+
+if [ -z "$DIST_DIR" ]; then
+  echo "Error: Empty DIST_DIR, exiting before attempting rm -rf"
+  exit 99
+else
+  rm -rf "$DIST_DIR"/*
+fi
+
+function createLogoVersions() {
+  LOGO_TYPE=$1
+
+  echo "Generating dist files for '$LOGO_TYPE'"
+
+  TARGET_DIR="$DIST_DIR/$LOGO_TYPE"
+
+  mkdir -p "$TARGET_DIR"
+
+  SRC_SVG="$SRC_DIR"/"$LOGO_TYPE".svg
+
+  echo -n "  Generating SVG version..."
+  TARGET_SVG="$TARGET_DIR"/"$LOGO_TYPE".svg
+  cp "$SRC_SVG" "$TARGET_SVG"
+  echo "OK"
+
+  echo "  Generating PNG versions..."
+  resize 16
+  resize 32
+  resize 64
+  resize 196
+  resize 256
+  resize 340
+  resize 368
+  resize 512
+  resize 768
+  resize 1024
+  resize 1920
+
+  echo ""
+}
+
+function resize() {
+  size=$1
+  density=$((size * 4))
+  echo -n "    $size"x...
+  docker run --rm --volume "$PWD/$SRC_SVG":"/app/input.svg" --volume "$PWD/$TARGET_DIR":"/output" --workdir "/app" acleancoder/imagemagick-full convert -background none -resize "$size"x -density "$density" -depth 8 input.svg /output/"$LOGO_TYPE"-"$size".png
+  echo "OK"
+}
+
+function optimizePng() {
+  DIR=$1
+
+  echo "Size before optimization"
+  du -h -d=0 "$DIR"
+
+  echo -n "Optimizing..."
+  CURRENT="$PWD"
+  cd "$DIR"
+  docker run --rm -v "$PWD":/source buffcode/docker-optipng -q -o7 **/*.png
+  cd "$CURRENT"
+  echo "OK"
+
+  echo "Size after optimization"
+  du -h -d=0 "$DIR"
+}
+
+createLogoVersions "crowdcode-logo"
+createLogoVersions "crowdcode-emblem"
+createLogoVersions "crowdcode-claim-logo"
+
+optimizePng "$DIST_DIR"
+
+bin/own.sh "$PWD/$DIST_DIR"
---
+++ readme.md
@@ -0,0 +1,6 @@
+# CROWDCODE Styleguide
+
+This repository contains everthing about the CROWDCODE Styleguide.
+
+
+<small>(C) Copyright by CROWDCODE GmbH &amp; Co. KG</small>
